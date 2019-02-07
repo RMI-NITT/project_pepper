6871dce9d0b0aefd87b84c7e5367281b1c00bc15
SubramanianKrish <subramanian.krish611@gmail.com>
2019-02-08 03:06:34 +0530

    encoder test

--- packages/pepper/scripts/odom_publish.py
+++ packages/pepper/scripts/odom_publish.py
@@ -5,11 +5,6 @@ import serial
 import math
 import time
 
-# Subject to change
-motor_left = serial.Serial("/dev/ttyUSB2", 9600, timeout=0.5)
-motor_right = serial.Serial("/dev/ttyUSB3", 9600, timeout=0.5)
-
-
 def motor_serial_write(motor_serial, command):
     """Method to write the command to the motors
 
@@ -18,7 +13,7 @@ def motor_serial_write(motor_serial, command):
     """
     # P - Read write encoder position in Rhino Motor RMCS220x series
     motor_serial.write(command + "\n\r")
-    time.sleep(0.2)
+    time.sleep(0.05)
     motor_serial.flushInput()
     motor_serial.flushOutput()
 
@@ -29,8 +24,8 @@ def motor_serial_read(motor_serial):
     :param motor_serial The serial port to read from
     """
     # This does something!
-    motor_serial.write("P" + '\n\r')
-    time.sleep(0.2)
+    motor_serial.write('P\n\r')
+    time.sleep(0.1)
     count = motor_serial.readline()
     motor_serial.flushInput()
     motor_serial.flushOutput()
@@ -41,37 +36,54 @@ def motor_serial_read(motor_serial):
     for i in count:
         if i.isdigit() or i == '-':
             ticks = ticks + i
-        if ticks != '':
-            return int(ticks)
-        else:
-            return -1
+    if ticks != '':
+        return int(ticks)
+    else:
+        return -1
 
 
 def main():
-    global motor_left, motor_right
+
+    # Subject to change
+    motor_left = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.5)
+    motor_right = serial.Serial("/dev/ttyUSB1", 9600, timeout=0.5)
+
     motor_left.flushOutput()
     motor_right.flushOutput()
+    motor_left.flushInput()
+    motor_right.flushInput()
 
-    time.sleep(5)
+    time.sleep(1)
     print "Ready!"
 
     # Initialize the motors to 0 encoder value
     motor_serial_write(motor_left, "P0")
-    #motor_serial_write(motor_right, "P0")
+    motor_serial_write(motor_right, "P0")
 
     # M200 sets the max speed of the motor to 200
     motor_serial_write(motor_left, "M200")
-    #motor_serial_write(motor_right, "M200")
+    motor_serial_write(motor_right, "M200")
 
     # R900 does a half rotation
-    motor_serial_write(motor_left, "R900")
-    #motor_serial_write(motor_right, "R900")
+    motor_serial_write(motor_left, "S100")
+    motor_serial_write(motor_right, "S100")
 
     print "Setup motor"
-    lt = motor_serial_read(motor_left)
-    #rt = motor_serial_read(motor_right)
+    
+    while True:
 
-    print lt#, rt
+        try:
+            lt = motor_serial_read(motor_left)
+            rt = motor_serial_read(motor_right) 
+
+            print lt, rt
+
+        except KeyboardInterrupt:
+            motor_serial_write(motor_left, "S0")
+            motor_serial_write(motor_right, "S0")
+            print "Stopping both motors!!"
+            break
+
 
 
 if __name__ == "__main__":
