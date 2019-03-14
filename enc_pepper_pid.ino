c01d91e9f02f04ac64b9c50367936dfb3f5e1641
Vidyaa <42498128+vidyaa29@users.noreply.github.com>
2019-03-14 12:58:06 +0530

    final pid code
    enc_pepper_pid file for new motors

--- enc_pepper_pid.ino
+++ enc_pepper_pid.ino
@@ -35,11 +35,11 @@ void setup() {
   pinMode(3,INPUT);   //EN_RB
   
   //Set Test PWM
-  analogWrite(Right_motor_pwm,100);
-  analogWrite(Left_motor_pwm,100);
+  analogWrite(Right_motor_pwm,30);
+  analogWrite(Left_motor_pwm,30);
 }
-
-double cur_enc_L= 0,kp = 0.8, ki = 0.01, cur_enc_R= 0,prev_enc_l=0,err_l = 0,err_r=0,prev_enc_r=0,vel_l =0,vel_r=0,rpm_l = 100,rpm_r = 100,val_l=2.5,val_r=2.5;
+double kil = 0,kir = 0,kpl = 0.031,kpr = 0.028; 
+double cur_enc_L= 0, cur_enc_R= 0,prev_enc_l=0,prev_enc_r=0,err_l = 0,err_r=0,i_err_r =0,i_err_l=0,vel_l =0,vel_r=0,rpm_l = 30,rpm_r = 30,val_l=1,val_r=1;
 void loop() {
   
   //Set Direction
@@ -55,20 +55,23 @@ void loop() {
   msg.point.x = cur_enc_L;
   msg.point.y = cur_enc_R;
  
-  // velocity = 2* 3.14* 0.052 * theta
+  // velocity = 2* 3.14* 0.052 * theta *255/100
   
-vel_l = ((cur_enc_L-prev_enc_l)*0.326)/(0.005*29520);
-  vel_r =((cur_enc_R- prev_enc_r)*0.326)/(0.005*29520);
+vel_l = ((cur_enc_L-prev_enc_l)*0.326*2.55)/(0.005*29520);
+  vel_r =((cur_enc_R- prev_enc_r)*0.326*2.55)/(0.005*29520);
   err_l = val_l - vel_l;
   err_r = val_r - vel_r;
-  
+  i_err_l += err_l;
+  i_err_r += err_r;
   //rpm_update
-  
-  rpm_l = rpm_l + err_l*kp;
-  rpm_r = rpm_r + err_r*kp;
+  if(abs(err_l) >0.05 || abs(err_r) > 0.05)
+  {
+  rpm_l = rpm_l + err_l*kpl + i_err_l*kil;//- 0.1*(vel_l - vel_r)/2;
+  rpm_r = rpm_r + err_r*kpr+ i_err_r*kir ;//+ 0.1* (vel_l - vel_r)/2;
+  }
   analogWrite(Right_motor_pwm,rpm_l);
   analogWrite(Left_motor_pwm,rpm_r);
-Serial.print(err_l);
+Serial.print(vel_r);
 Serial.print(' ');
 Serial.println(vel_l);
   prev_enc_l = cur_enc_L;
